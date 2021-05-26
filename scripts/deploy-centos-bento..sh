d367369699843a903741c1931e06ed5ce6594501
Your Name <you@example.com>
2021-05-26 14:24:48 +0300

    add bento

--- scripts/deploy-centos-bento..sh
+++ scripts/deploy-centos-bento..sh
@@ -9,12 +9,15 @@ echo "==========================================================================
 vagrant plugin install vagrant-libvirt #The vagrant-libvirt plugin is required when using KVM on Linux
 vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with different providers
 
-# https://app.vagrantup.com/centos/boxes/8
-vagrant box add "centos/8" --provider=libvirt
+# https://app.vagrantup.com/bento/boxes/centos-8.3
+
+
+vagrant box add  "bento/centos-8.3" --provider=virtualbox
+vagrant mutate "bento/centos-8.3"  libvirt
 
 vagrant init --template Vagrantfile.provision.bash.erb
 # must be created in project root directory with Vagrantfile template file
-vagrant up --provider=libvirt "vg-centos-03"
+vagrant up --provider=libvirt "vg-centos-04"
 
 vagrant box list #veridy installed boxes
 virsh list --all #show all running KVM/libvirt VMs
