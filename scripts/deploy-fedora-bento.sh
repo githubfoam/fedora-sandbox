8e5be579e8d202cca68d5dc312166d21673ee914
Your Name <you@example.com>
2021-05-26 11:49:58 +0300

    init bento

--- scripts/deploy-fedora-bento.sh
+++ scripts/deploy-fedora-bento.sh
@@ -9,13 +9,15 @@ echo "==========================================================================
 vagrant plugin install vagrant-libvirt #The vagrant-libvirt plugin is required when using KVM on Linux
 vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with different providers
 
-# https://app.vagrantup.com/fedora/boxes/34-cloud-base
-# vagrant box add "fedora/32-cloud-base" --provider=libvirt
-# vagrant box add "fedora/33-cloud-base" --provider=libvirt
-vagrant box add "fedora/34-cloud-base" --provider=libvirt
+# https://github.com/chef/bento/tree/master/packer_templates/fedora
+# https://app.vagrantup.com/bento/boxes/fedora-33
+
+vagrant box add  "bento/fedora-33" --provider=virtualbox
+vagrant mutate "bento/fedora-33"  libvirt
+
 vagrant init --template Vagrantfile.provision.bash.erb
 # must be created in project root directory with Vagrantfile template file
-vagrant up --provider=libvirt "vg-fedora-02"
+vagrant up --provider=libvirt "vg-fedora-01"
 
 vagrant box list #veridy installed boxes
 virsh list --all #show all running KVM/libvirt VMs
